936752f5d14cc79e50d2aab8c6f09881246996ad
Alex Huang <109326746+alexziao05@users.noreply.github.com>
2024-10-12 22:42:46 -0700

    Initial Code

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/ziaohuang/CLionProjects/HeapSort
+# Install script for directory: /Users/ziaohuang/Documents/GitHub/Heap-Sort-On-Arrays
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/ziaohuang/CLionProjects/HeapSort/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/ziaohuang/Documents/GitHub/Heap-Sort-On-Arrays/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
