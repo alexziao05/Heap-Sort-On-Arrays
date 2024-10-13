40ace95dacd0d807045e614088086fee8c967f30
Alex Huang <109326746+alexziao05@users.noreply.github.com>
2024-10-13 00:07:43 -0700

    First Implementation

--- main.cpp
+++ main.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
 using namespace std;
 
+void PercolateUp (int currIndex, int arr[]) {
+    while (currIndex > 0) {
+        int parent = (currIndex - 1) / 2;
 
-void heapify(int arr[], int n) {
-    // Your code here
-    // Build the heap
+        if (arr[currIndex] >= arr[parent]) {
+            // No violation
+            return;
+        } else {
+            swap (arr[currIndex], arr[parent]);
+            currIndex = parent;
+        }
+    }
+}
+
+void swap (int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void heapify (int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        PercolateUp (i, arr);
+    }
 }
 
 // Main function to perform heap sort on the array
-void heapSort(int arr[], int n) {
+void heapSort (int arr[], int n) {
     // Step 1: Build a max-heap
 
     heapify(arr, n);
@@ -22,7 +42,7 @@ void heapSort(int arr[], int n) {
 
 
 // Function to print the array
-void printArray(int arr[], int n) {
+void printArray (int arr[], int n) {
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
@@ -35,6 +55,10 @@ int main() {
     int n = sizeof(arr) / sizeof(arr[0]);
 
     cout << "Original array:\n";
+    printArray(arr, n);
+
+    cout << "Heapify array:\n";
+    heapSort(arr, n);
     printArray(arr, n);
 
     // Call the heapSort function
